3eea1c0ab7c7c28d1a2b31e2dab63bed70e6b94c
marius booysen <marius@booysen>
2019-05-02 19:55:41 +0200

    Adding tests model and static url

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,9 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
 
+class test(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    dob =models.DateField(null=True)
+  
+
