408322cfdf9d50550407d27e5ab89dac3ea07ec9
marius booysen <marius@booysen>
2019-05-05 15:35:55 +0200

    Updated blog model

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=250)
     pubdate = models.DateField(null=True)
-    body = models.TextField  
+    body = models.TextField(null=True) 
     image = models.ImageField(upload_to='images/') 
