c96261c745d5966b4abac33ac44d2ddc8a3721a5
marius booysen <marius@booysen>
2019-05-07 20:58:26 +0200

    Added blog retrieval

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,13 @@ class Blog(models.Model):
     title = models.CharField(max_length=250)
     pubdate = models.DateField(null=True)
     body = models.TextField(null=True) 
-    image = models.ImageField(upload_to='images/') 
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:10]
+
+    def pub_date_pretty(self):
+        return self.pubdate.strftime('%Y-%m-%d')
